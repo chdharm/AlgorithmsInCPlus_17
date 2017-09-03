146356e2180a738218eac9e795bbbb7795ea7104
chdharm <chaudharydharmpal95@gmail.com>
2017-09-03 17:10:11 +0530

    Modifying fibonacci series program

--- 11-Jun-01/fib-me.cpp
+++ 11-Jun-01/fib-me.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int fib(int n){
 	//base case
 	if (n == 0) return 0;
-	if (n == 1) return 2;
+	if (n == 1) return 1;
 	//rescursive step
 	int nth = fib(n - 1) + fib(n - 2);
 	return nth;
